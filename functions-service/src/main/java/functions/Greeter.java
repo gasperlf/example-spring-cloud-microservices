0282278042e6f87c86bde3017c60c4ee58659928
gasperlf <lewis.florez@globant.com>
2018-11-30 15:58:14 -0500

    add project functions like as service with some simpliest examples

---
+++ functions-service/src/main/java/functions/Greeter.java
@@ -0,0 +1,9 @@
+package functions;
+
+import java.util.function.Function;
+
+public class Greeter implements Function<String, String> {
+    public String apply(String name) {
+        return "Hello " + name;
+    }
+}
